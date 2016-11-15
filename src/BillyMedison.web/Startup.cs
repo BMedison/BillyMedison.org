17c040b8aab2debe57772a2c3562f5689ac72779
Billy Medsion <billy.medison@outlook,.com>
2016-11-15 11:00:24 +0300

    Initial commit

--- src/BillyMedison.web/Startup.cs
+++ src/BillyMedison.web/Startup.cs
@@ -28,10 +28,12 @@ namespace BillyMedison.web
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+//            app.Run(async (context) =>
+//            {
+//                await context.Response.WriteAsync("Hello World!");
+//            });
         }
     }
 }
