093eccbcc5baf3dad4d6c4a78d16f024db4f9139
Billy Medsion <billy.medison@outlook,.com>
2016-11-15 18:11:02 +0300

    angular was added

---
+++ src/BillyMedison.web/wwwroot/app/home/home_controller.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('home_controller', home_controller);
+
+    home_controller.$inject = ['$location']; 
+
+    function home_controller($location) {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.title = 'home_controller';
+
+        activate();
+
+        function activate() { }
+    }
+})();
