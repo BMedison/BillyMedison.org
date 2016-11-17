1c9303873346edeb165038d5638ede0952f4a909
Billy Medsion <billy.medison@outlook.com>
2016-11-17 20:33:38 +0300

    *

--- src/site/wwwroot/app/home/home.js
+++ src/site/wwwroot/app/home/home.js
@@ -20,6 +20,16 @@
         $('.blue.item').removeClass('active')
         $('a#home_mnu.blue.item').addClass('active');
 
+        vm.topics = [
+            { url: "https://go.microsoft.com/fwlink/?LinkID=398600", name: "Add a Controller and View" },
+            { url: "https://go.microsoft.com/fwlink/?LinkID=699314", name: "Add an appsetting in config and access it in app." },
+            { url: "https://go.microsoft.com/fwlink/?LinkId=699315", name: "Manage User Secrets using Secret Manager." },
+            { url: "https://go.microsoft.com/fwlink/?LinkId=699316", name: "Use logging to log a message." },
+            { url: "https://go.microsoft.com/fwlink/?LinkId=699317", name: "Add packages using NuGet." },
+            { url: "https://go.microsoft.com/fwlink/?LinkId=699318", name: "Add client packages using Bower." },
+            { url: "https://go.microsoft.com/fwlink/?LinkId=699319", name: "Target development, staging or production environment." }
+        ];        
+
         activate();
 
         function activate() { }
